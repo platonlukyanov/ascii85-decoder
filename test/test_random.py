145363856329bafaab660a537470b33c5ed75c30
Platon Lukyanov <platonioluka@gmail.com>
2025-04-16 16:21:50 +0300

    Added randomized testing set

--- test/test_random.py
+++ test/test_random.py
@@ -1,8 +1,20 @@
 import unittest
 import base64
 import subprocess
+import random
+import string
 
 ASCII85_EXEC_PATH = './ascii85'
+
+RANDOM_SEED = 42;
+
+random.seed(RANDOM_SEED)
+
+def generate_random_string():
+    length = random.randint(1, 20)
+    alphabet = string.ascii_letters + 'йцукенгшщзхъфывапролджэячсмитьбю12345'
+    word = ''.join(random.choice(alphabet) for _ in range(length))
+    return word
 
 def encode_to_ascii85_with_exec(text: str):
     result = subprocess.run([ASCII85_EXEC_PATH], input=text, capture_output=True, text=True)
@@ -23,6 +35,10 @@ class TestASCII85Exec(unittest.TestCase):
     def test_encoding(self):
         test_cases = ["Hello", "Привет", "", "123", "У меня есть 5% от всей компании", "!@#$%^"]
 
+        
+        random_test_cases = [generate_random_string() for i in range(100)]
+        test_cases.extend(random_test_cases)
+
         for test_string in test_cases:
             with self.subTest(text=test_string):
                 expected_ascii85 = base64.a85encode(test_string.encode("utf8")).decode('ascii')
@@ -32,6 +48,8 @@ class TestASCII85Exec(unittest.TestCase):
 
     def test_decoding_positive(self):
         test_original = ["Hello", "Привет", "123", "У меня есть 5% от всей компании", "!@#$%^"]
+        random_test_cases = [generate_random_string() for i in range(100)]
+        test_original.extend(random_test_cases)
         test_cases = list(map(lambda x: base64.a85encode(x.encode("utf-8")).decode('ascii'), test_original))
     
         for original, encoded in zip(test_original, test_cases):
@@ -50,4 +68,4 @@ class TestASCII85Exec(unittest.TestCase):
     
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main()    
