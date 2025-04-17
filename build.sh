7726ea70d980df3e3e9aa193ddc7b5d0239a8325
Platon Lukyanov <platonioluka@gmail.com>
2025-04-17 16:19:38 +0300

    Debuggin CI

--- build.sh
+++ build.sh
@@ -4,14 +4,14 @@
 GXX_VERSION="11"
 
 build_app() {
-    g++-"${GXX_VERSION}" -std=c++17 -o ascii85 main.cpp
+    g++-"${GXX_VERSION}" -v -std=c++17 -o ascii85 main.cpp
 }
 
 build_tests() {
-    g++-"${GXX_VERSION}" -std=c++17 -DTESTING -o tests test/test.cpp main.cpp \
+    g++-"${GXX_VERSION}" -v -std=c++17 -DTESTING -o tests test/test.cpp main.cpp \
         -lgtest -lgtest_main -lpthread
 }
-
+pwd
 case "$1" in
     "test") build_tests ;;
     *) build_app ;;
